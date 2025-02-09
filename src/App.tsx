0828031500f16446bdfd878366976ac613bc15b0
Andrew Zmeevskiy <punkz666@gmail.com>
2025-02-09 11:21:33 +0300

    feat: add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './view/pages/main/main';
+import { NotFound } from './view/pages/notfound/notfound';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
