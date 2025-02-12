9bfd687c302f403435d7fc7cf54a3a767163774d
Andrew Zmeevskiy <punkz666@gmail.com>
2025-02-13 00:27:25 +0300

    fix: pagination all hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useSearchParams,
+} from 'react-router-dom';
 import './App.css';
 import Main from './view/pages/main/main';
 import { NotFound } from './view/pages/notfound/notfound';
 
 function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <Main
+              searchParams={searchParams}
+              setSearchParams={setSearchParams}
+            />
+          }
+        />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
